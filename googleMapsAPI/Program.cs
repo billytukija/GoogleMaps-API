39f9e2cf49a01301dc642f5302daca04e57ddc3e
Billy Tukija Richard <billy.tukija@onlineapp.com.br>
2020-02-13 16:15:19 -0300

    Processo para retornar places usando Google maps api

---
+++ googleMapsAPI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Net;
+
+namespace googleMapsAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name,rating&key=AIzaSyDvjchyqGe0vRHFDPNSKAHjD-zjt38liqc";
+
+            var req = WebRequest.Create(url);
+            var res = req.GetResponse();
+
+            var data = res.GetResponseStream();
+            var reader = new StreamReader(data);
+
+            var resFromServer = reader.ReadToEnd();
+
+            res.Close();
+        }
+    }
+}
