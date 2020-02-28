d376da75da4a9d1617af3659d4af96565ac22f53
billytukija <31943520+billytukija@users.noreply.github.com>
2020-02-28 16:16:41 -0300

    Update Program.cs

--- googleMapsAPI/Program.cs
+++ googleMapsAPI/Program.cs
@@ -7,7 +7,7 @@ namespace googleMapsAPI
     {
         static void Main(string[] args)
         {
-            var url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name,rating&key=AIzaSyDvjchyqGe0vRHFDPNSKAHjD-zjt38liqc";
+            var url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name,rating&key=YOUR_KEY";
 
             var req = WebRequest.Create(url);
             var res = req.GetResponse();
